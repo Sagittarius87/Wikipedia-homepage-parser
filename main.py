29441fb92b85adea30c6de2f19e90ba1a5fe5583
Sagittarius87 <Sagittarius87@Jarik-87@yandex.ru>
2023-05-10 20:39:35 +0300

    Исправил получение текста статьи, теперь находятся все видимые абзацы избранной статьи

--- main.py
+++ main.py
@@ -18,25 +18,25 @@ def get_html(url):
 def get_data(html):
     print('get_data start')
     soup_of_html = BeautifulSoup(html, 'lxml')
-
     
     selected_article = soup_of_html.find('div', id='main-tfa').find('div').text
-    article_title = soup_of_html.find('div', id='main-tfa').find('h2').find('span', class_='mw-headline').find('a').text
-    article_text = soup_of_html.find('div', id='main-tfa').find('p').text
-    
-    print('---')
     print(selected_article)
+    article_title = soup_of_html.find('div', id='main-tfa').find('h2').find('span', class_='mw-headline').find('a').text
     print(article_title)
-    print(article_text)
-    print('---')
-    
+    get_article_text = soup_of_html.find('div', id='main-tfa').find_all('p')
+    for p in get_article_text:
+        artical_text = p.text
+        print(artical_text)
+
+
+
     return selected_article
 
 # Основная функция, отправляющая запрос и разбирающая полученные данные из html кода
 def main():
     print('main start')
     url = 'https://ru.wikipedia.org/wiki/Заглавная_страница'
-    print(get_data(get_html(url)))
+    get_data(get_html(url))
 
 
 # Проверка значения __name__ при запуске исходного файла как основной программы и запуск функции main() в случае True
