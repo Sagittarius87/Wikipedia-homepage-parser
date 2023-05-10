67d22e6b7c0043cc9a20e7a059b98d1e96cf6e0a
Sagittarius87 <Sagittarius87@Jarik-87@yandex.ru>
2023-05-10 20:06:02 +0300

    Добавил комментарии

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from unittest.main import main
 import requests
 from bs4 import BeautifulSoup
 
+
+# Парсер главной страницы википедии
+# https://ru.wikipedia.org/wiki/Заглавная_страница
+
+
+# Отправка запроса для получения html кода главной страницы
 def get_html(url):
     print('get_html start')
     resp = requests.get(url)
     return resp.text
 
-
+# Создание экземпляра класса, содержащего данные из полученного html кода страницы, и поиск нужных элементов
 def get_data(html):
     print('get_data start')
     soup_of_html = BeautifulSoup(html, 'lxml')
@@ -26,10 +32,13 @@ def get_data(html):
     
     return div_main_box_subtitle
 
+# Основная функция, отправляющая запрос и разбирающая полученные данные из html кода
 def main():
     print('main start')
     url = 'https://ru.wikipedia.org/wiki/Заглавная_страница'
     print(get_data(get_html(url)))
 
+
+# Проверка значения __name__ при запуске исходного файла как основной программы и запуск функции main() в случае True
 if __name__ == '__main__':
     main()
